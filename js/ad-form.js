9412449aef0f4f8a988b12bc9727cc316ad9bfc1
alex-home <sakun.alexey@gmail.com>
2021-02-22 17:01:20 +0200

    Implement form validation

--- js/ad-form.js
+++ js/ad-form.js
@@ -7,6 +7,10 @@ import {
 const DEACTIVATION_CLASSNAME = 'ad-form--disabled';
 const INTERACTIVE_ELEMENTS_SELECTOR = 'fieldset';
 const AD_ADDRESS_PRECISION = 5;
+const MIN_AD_TITLE_LENGTH = 30;
+const MAX_AD_TITLE_LENGTH = 100;
+const MAX_AD_PRICE = 1000000;
+const MAX_ROOMS_NUMBER = 100;
 
 const adForm = document.querySelector('.ad-form');
 const adType = adForm.querySelector('#type');
@@ -14,6 +18,9 @@ const adPrice = adForm.querySelector('#price');
 const adTimein = adForm.querySelector('#timein');
 const adTimeout = adForm.querySelector('#timeout');
 const adAddress = adForm.querySelector('#address');
+const adTitle = adForm.querySelector('#title');
+const adRoomsNumber = adForm.querySelector('#room_number');
+const adCapacity = adForm.querySelector('#capacity');
 
 const onAdTypeChange = () => {
   const minPrice = adTypeToMinPrice[adType.value];
@@ -58,11 +65,56 @@ const setAdAddress = (lat, lng) => {
   adAddress.value = `${lat.toFixed(AD_ADDRESS_PRECISION)}, ${lng.toFixed(AD_ADDRESS_PRECISION)}`;
 }
 
+adTitle.addEventListener('input', () => {
+  const valueLength = adTitle.value.length;
+
+  if (adTitle.validity.tooShort) {
+    adTitle.setCustomValidity(`Введите еще ${MIN_AD_TITLE_LENGTH - valueLength} симв.! \
+    Введено ${valueLength} симв.`);
+  } else if (adTitle.validity.tooLong) {
+    adTitle.setCustomValidity(`Удалите ${valueLength - MAX_AD_TITLE_LENGTH} симв. \
+    Введено ${valueLength} симв.`);
+  } else {
+    adTitle.setCustomValidity('');
+  }
+
+  adTitle.reportValidity();
+});
+
+adPrice.addEventListener('input', () => {
+
+  if (adPrice.validity.rangeOverflow) {
+    adPrice.setCustomValidity(`Максимальная цена не может быть больше ${MAX_AD_PRICE}!`);
+  } else {
+    adPrice.setCustomValidity('');
+  }
+
+  adPrice.reportValidity();
+});
+
+const updateAdCapacityItems = (roomsNumber) => {
+
+  for (const capacityItem of adCapacity.children) {
+
+    if (roomsNumber >= MAX_ROOMS_NUMBER) {
+      capacityItem.disabled = capacityItem.value !== '0' ? true : false;
+    } else {
+      capacityItem.disabled = (capacityItem.value > roomsNumber) || (capacityItem.value === '0') ? true : false;
+    }
+  }
+}
+
+adRoomsNumber.addEventListener('change', (evt) => {
+  updateAdCapacityItems(evt.target.value);
+})
+
+
 onAdTypeChange();
 onAdTimeinChange();
 onAdTimeoutChange();
 deactivateAdForm();
 setAdAddress(TOKIYO_GEO_POSITON.latitude, TOKIYO_GEO_POSITON.longitude);
+updateAdCapacityItems(adRoomsNumber.value);
 adType.addEventListener('change', onAdTypeChange);
 adTimein.addEventListener('change', onAdTimeinChange);
 adTimeout.addEventListener('change', onAdTimeoutChange);
